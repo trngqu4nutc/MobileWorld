c064e5801b8bb14bf385c6327ca2b3dafae86b33
quan le <trngqu4n2110@outlook.com>
2020-04-20 21:35:27 +0700

    changed basket

--- app/models/baskets.model.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, Sequelize) => {
-    const Baskets = sequelize.define("Baskets", {
-        buyerid: {
-            type: Sequelize.INTEGER,
-            allowNull: false
-        }
-    });
-    return Baskets;
-};
